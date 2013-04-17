ab7bf01b96c980dcc5a8928948867aa8d8ce3a38
Erik Kamp <ekamp@basic.rutgers.edu>
2013-04-16 20:31:39 -0400

    Added function definitions for the functions listed under HTTP functions

---
+++ httpRelatedFunctions.java
@@ -0,0 +1,50 @@
+public class httpRelatedFunctions
+{
+/***Need to create a peer object in order to store peer data and references ...***/
+
+
+	/*Adds new content to the server */
+	/*Sends certain data to a certain peer which will be contacted through reference*/
+
+	private boolean put (String data, Peer peerReference)
+	{
+		//dat code!!!
+		return true;
+	}
+
+	/*returns data associated with the cooresponding URL*/
+
+	private data get (String URL)
+	{
+		//can be redirected
+		return null;
+	}
+
+	/*Opposite of PUT , basically deletes certain data from the server */
+	/* Again here the data is the data and the reference is the reference to the server where you want to delete data from */
+	private boolean delete (String data , Peer peerReference)
+	{
+		//Code lah!!!
+		return true;
+	}
+
+	/*Text dump of all data stored at a specified peer */
+	private String[] list (Peer peerReference)
+	{
+		//  -_-  :/ :) ^_^ :p
+		return null;
+	}
+
+	/*Text dump of all peer information that one peer has on other peers */
+	private String[] (Peer peerReference)
+	{
+		//Basically loop through all peer information and either print to screen or dump in an array of some kind
+	}
+
+	/*Removes a peer from a group */
+	/***Have to create datastructure for the peer groups ***/
+	private boolean remove (Peer peerReference,PeerStructureGroup group)
+	{
+	/*loop or hash to find the correct peer and remove it from the data structure */
+	}
+
